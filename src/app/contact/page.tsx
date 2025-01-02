1f5d5a0cb0e904cb504767e0e4e3c9f4773b5c4e
mdm17 <diky.maulana88@siberat.id>
2025-01-02 11:56:59 +0700

    feat contact

--- src/app/contact/page.tsx
+++ src/app/contact/page.tsx
@@ -3,6 +3,54 @@ import React from 'react'
 
 export default function Contact() {
   return (
-    <div>Contact - Update</div>
+    <div className="relative max-w-4xl mx-auto">
+      <div className="grid grid-cols-6 h-full w-full">
+        <div className="bg-black  p-10 col-span-2 rounded-l-lg">
+          <h2 className="mb-10 font-bold text-2xl text-blue-100 before:block before:absolute before:bg-sky-300 before:content[''] relative before:w-20 before:h-1 before:-skew-y-3 before:-bottom-4">Info Kontak</h2>
+          <p className="font-bold text-blue-100 py-8 border-b border-blue-700">
+            Alamat
+            <span className="font-normal text-xs text-white block">Palembang, Indonesia</span>
+          </p>
+          <p className="font-bold text-blue-100 py-8 border-b border-blue-700">
+            No. Telpon
+            <span className="font-normal text-xs text-white block">+62 896 4343 1414</span>
+          </p>
+          <p className="font-bold text-blue-100 py-8 border-b border-blue-700">
+            Email
+            <span className="font-normal text-xs text-white block">diky.maulana88@gmail.com</span>
+          </p>
+          {/* <p className="font-bold text-blue-100 py-8 border-b border-blue-700">
+            Sosial Media
+            <span className="font-normal text-xs text-white block">zigzagexampl.com</span>
+          </p> */}
+
+        </div>
+        <div className=" bg-black bg-opacity-40 dark:bg-white dark:bg-opacity-20 p-14 col-span-4 rounded-r-lg">
+          <h2 className="mb-14 font-bold text-4xl text-slate-300 before:block before:absolute before:bg-sky-300 before:content[''] relative before:w-20 before:h-1 before:-skew-y-3 before:-bottom-4">Hubungi Saya</h2>
+          <div className="grid gap-6 mb-6 grid-cols-2">
+            <div className="flex flex-col">
+              <input className="py-4 bg-gray-600 dark:bg-gray-400 rounded-full px-6 placeholder:text-xs" aria-placeholder="Votre nom" placeholder="Nama" />
+            </div>
+            <div className="flex flex-col">
+              <input className="py-4 bg-gray-600 dark:bg-gray-400 rounded-full px-6 placeholder:text-xs" aria-placeholder="Votre nom" placeholder="No HP" />
+            </div>
+          </div>
+          <div className="grid gap-6 mb-6 grid-cols-2">
+            <div className="flex flex-col">
+              <input className="py-4 bg-gray-600 dark:bg-gray-400 rounded-full px-6 placeholder:text-xs" aria-placeholder="Votre nom" placeholder="Email" />
+            </div>
+            <div className="flex flex-col">
+              <input className="py-4 bg-gray-600 dark:bg-gray-400 rounded-full px-6 placeholder:text-xs" aria-placeholder="Votre nom" placeholder="Subject" />
+            </div>
+          </div>
+          <div className="mb-6">
+            <textarea className="w-full bg-gray-600 dark:bg-gray-400 rounded-2xl placeholder:text-xs px-6 py-4" placeholder="Pesan" name="" id="" rows={8}></textarea>
+          </div>
+          <div className="flex justify-center">
+            <button className="rounded-full bg-blue-900 text-white font-bold py-4 px-6 min-w-40 hover:bg-blue-800 transition-all">Kirim</button>
+          </div>
+        </div>
+      </div>
+    </div>
   )
 }
--- src/app/page.tsx
+++ src/app/page.tsx
@@ -14,22 +14,25 @@ export default function Home() {
         <h1 className="text-3xl pt-5 text-slate-300 font-semibold text-center">
           M Dicky Maulana
         </h1>
-        <p className="xl:w-1/2 m-3 mx-auto text-white text-center text-wrap">
-          Saya adalah seorang Fullstack Developer berpengalaman &#xB1; 3 tahun di industri logistik, berdomisili di Palembang.
-          <br />
-          Saat ini saya bekerja di <Link href={'https://siberat.id/'} target="_blank">Siberat Digital Logistik</Link> sebagai Engineering Senior Officer.
-          <br />
-          Berbekal keahlian dalam JavaScript,
-          PHP, SQL, dan Linux OS, serta memiliki pengalaman dalam pengembangan aplikasi menggunakan tools dan frameworks yang populer seperti
-          &nbsp;<Link href={'https://laravel.com/'} target="_blank">Laravel</Link>,
-          &nbsp;<Link href={'https://expressjs.com/'} target="_blank">ExpressJS</Link>,
-          &nbsp;<Link href={'https://nextjs.org/'} target="_blank">NextJS</Link>,
-          &nbsp;<Link href={'https://github.com/'} target="_blank">Git</Link>, dan
-          &nbsp;<Link href={'https://www.docker.com/'} target="_blank">Docker</Link>.
-          <br />
-          Saya juga memiliki semangat yang tinggi untuk mempelajari teknologi baru dan terus meningkatkan keahlian guna menghadapi tantangan industri yang dinamis.
-          Berbekal pengalaman dan kemampuan ini, saya percaya dapat memberikan kontribusi signifikan dalam menciptakan proyek-proyek inovatif dan solusi berdampak nyata.
-        </p>
+        <div className="xl:w-1/2 m-3 mx-auto text-white text-wrap grid gap-y-5">
+          <p>
+            Saya adalah seorang Fullstack Developer berpengalaman &#xB1; 3 tahun di industri logistik, berdomisili di Palembang.
+            <br />
+            Saat ini saya bekerja di <Link className="text-blue-500" href={"https://siberat.id/"} target="_blank">Siberat Digital Logistik</Link> sebagai Engineering Senior Officer.
+          </p>
+          <p>
+            Berbekal keahlian dalam JavaScript,
+            PHP, SQL, dan Linux OS, serta memiliki pengalaman dalam pengembangan aplikasi menggunakan tools dan frameworks yang populer seperti
+            &nbsp;<Link className="text-blue-500" href={"https://laravel.com/"} target="_blank">Laravel</Link>,
+            &nbsp;<Link className="text-blue-500" href={"https://expressjs.com/"} target="_blank">ExpressJS</Link>,
+            &nbsp;<Link className="text-blue-500" href={"https://nextjs.org/"} target="_blank">NextJS</Link>,
+            &nbsp;<Link className="text-blue-500" href={"https://github.com/"} target="_blank">Git</Link>, dan
+            &nbsp;<Link className="text-blue-500" href={"https://www.docker.com/"} target="_blank">Docker</Link>.
+          </p>
+          <p>Saya juga memiliki semangat yang tinggi untuk mempelajari teknologi baru dan terus meningkatkan keahlian guna menghadapi tantangan industri yang dinamis.
+            Berbekal pengalaman dan kemampuan ini, saya percaya dapat memberikan kontribusi signifikan dalam menciptakan proyek-proyek inovatif dan solusi berdampak nyata.
+          </p>
+        </div>
       </div>
     </div>
   );
